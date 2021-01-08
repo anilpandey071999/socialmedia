2ef1931014db5520c5a779c82028eb8bfb04e34e
anilpandey071999 <31802461+anilpandey071999@users.noreply.github.com>
2021-01-08 15:33:57 +0530

    post page

--- lib/widgets/post.dart
+++ lib/widgets/post.dart
@@ -1,13 +1,209 @@
+import 'dart:ui';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:socalnetwork/models/user.dart';
+import 'package:socalnetwork/pages/home.dart';
+import 'package:socalnetwork/widgets/progress.dart';
 
 class Post extends StatefulWidget {
+  final String postId;
+  final String ownerId;
+  final String userName;
+  final String location;
+  final String description;
+  final String mediaUrl;
+  final dynamic likes;
+
+  Post({
+    this.postId,
+    this.ownerId,
+    this.userName,
+    this.location,
+    this.description,
+    this.mediaUrl,
+    this.likes,
+  });
+
+  factory Post.fromDocument(DocumentSnapshot doc) {
+    return Post(
+      postId: doc['postId'],
+      ownerId: doc['ownerId'],
+      userName: doc['userName'],
+      location: doc['location'],
+      description: doc['description'],
+      mediaUrl: doc['mediaUrl'],
+      likes: doc['likes'],
+    );
+  }
+
+  int getLikesCount(likes) {
+    if (likes == null) {
+      return 0;
+    }
+    int count = 0;
+    likes.values.forEach((val) {
+      if (val == true) {
+        count += 1;
+      }
+    });
+    return count;
+  }
+
   @override
-  _PostState createState() => _PostState();
+  _PostState createState() => _PostState(
+      postId: this.postId,
+      ownerId: this.ownerId,
+      userName: this.userName,
+      location: this.location,
+      description: this.description,
+      mediaUrl: this.mediaUrl,
+      likes: this.likes,
+      likeCount: getLikesCount(this.likes));
 }
 
 class _PostState extends State<Post> {
+  final String postId;
+  final String ownerId;
+  final String userName;
+  final String location;
+  final String description;
+  final String mediaUrl;
+  int likeCount;
+  Map likes;
+
+  _PostState({
+    this.postId,
+    this.ownerId,
+    this.userName,
+    this.location,
+    this.description,
+    this.mediaUrl,
+    this.likes,
+    this.likeCount,
+  });
+
+  buildPostHeader() {
+    return FutureBuilder(
+      future: userRef.doc(ownerId).get(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return circularProgress();
+        }
+        User user = User.fromDocument(snapshot.data);
+        return ListTile(
+          leading: CircleAvatar(
+            backgroundColor: Colors.grey,
+            backgroundImage: CachedNetworkImageProvider(user.photoUrl),
+          ),
+          title: GestureDetector(
+            onTap: () => print("showing Profile"),
+            child: Text(
+              user.username,
+              style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          subtitle: Text(location),
+          trailing: IconButton(
+            onPressed: () => print('Delete Post'),
+            icon: Icon(Icons.more_vert),
+          ),
+        );
+      },
+    );
+  }
+
+  buildPostImage() {
+    return GestureDetector(
+      onDoubleTap: () => print("liking the post"),
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          Image.network(mediaUrl),
+        ],
+      ),
+    );
+  }
+
+  buildPostFooter() {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Padding(
+              padding: EdgeInsets.only(top: 40.0, left: 20.0),
+            ),
+            GestureDetector(
+              onTap: () => print("like post"),
+              child: Icon(
+                Icons.favorite_border,
+                size: 28.0,
+                color: Colors.pink,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(right: 20.0),
+            ),
+            GestureDetector(
+              onTap: () => print("Comment on the post"),
+              child: Icon(
+                Icons.mode_comment,
+                size: 28.0,
+                color: Colors.blue[900],
+              ),
+            ),
+          ],
+        ),
+        Row(
+          children: [
+            Container(
+              margin: EdgeInsets.only(left: 20.0),
+              child: Text(
+                "$likeCount likes",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            )
+          ],
+        ),
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              margin: EdgeInsets.only(left: 20.0),
+              child: Text(
+                "$userName ",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Expanded(
+              child: Text(description),
+            )
+          ],
+        )
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text("Post");
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        buildPostHeader(),
+        buildPostImage(),
+        buildPostFooter()
+      ],
+    );
   }
 }
