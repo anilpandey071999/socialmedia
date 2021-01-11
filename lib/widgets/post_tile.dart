219f4a095eee4190c9db007d4ff0340639c28cd7
anilpandey071999 <31802461+anilpandey071999@users.noreply.github.com>
2021-01-11 11:47:45 +0530

    post tiles

--- lib/widgets/post_tile.dart
+++ lib/widgets/post_tile.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:socalnetwork/widgets/custom_image.dart';
+import 'package:socalnetwork/widgets/post.dart';
 
 class PostTile extends StatelessWidget {
+  final Post post;
+
+  PostTile(this.post);
   @override
   Widget build(BuildContext context) {
-    return Text("Post Tile");
+    return GestureDetector(
+      onTap: () => print("Full screen"),
+      child: cachedNetworkImage(post.mediaUrl),
+    );
   }
 }
