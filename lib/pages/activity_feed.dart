03fe83cc5e0ec80264638e4c0c8b1a6e629e149b
anilpandey071999 <31802461+anilpandey071999@users.noreply.github.com>
2021-01-15 03:14:38 +0530

    activity feed

--- lib/pages/activity_feed.dart
+++ lib/pages/activity_feed.dart
@@ -1,5 +1,10 @@
+import 'package:timeago/timeago.dart' as timeago;
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:socalnetwork/pages/home.dart';
 import 'package:socalnetwork/widgets/header.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:socalnetwork/widgets/progress.dart';
 
 class ActivityFeed extends StatefulWidget {
   @override
@@ -7,25 +12,160 @@ class ActivityFeed extends StatefulWidget {
 }
 
 class _ActivityFeedState extends State<ActivityFeed> {
+  getActiviyFeed() async {
+    // print("Activity Feed is starting featching.......... ");
+    QuerySnapshot snapshot = await activityFeedRef
+        .doc(currentUser.id)
+        .collection('feedItems')
+        .orderBy('timestamp', descending: true)
+        .limit(50)
+        .get();
+    // print("add to list");
+    List<ActivityFeedItem> feedItem = [];
+    snapshot.docs.forEach((doc) {
+      feedItem.add(ActivityFeedItem.fromDocument(doc));
+      // print("Activity Feeds: ${doc.data()}");
+    });
+    return feedItem;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: header(
-        context,
-        isAppTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          "Activity Feed",
+        appBar: header(
+          context,
+          titleText: "Activity Feed",
         ),
-      ),
-    );
+        body: Container(
+          child: FutureBuilder(
+            future: getActiviyFeed(),
+            builder: (context, snapshot) {
+              if (!snapshot.hasData) {
+                return circularProgress();
+              }
+              return ListView(
+                children: snapshot.data,
+              );
+            },
+          ),
+        ));
   }
 }
 
+Widget mediaPreview;
+String activityItemText;
+
 class ActivityFeedItem extends StatelessWidget {
+  final String username;
+  final String userId;
+  final String type;
+  final String mediaUrl;
+  final String postId;
+  final String userProfileImg;
+  final String commenData;
+  final Timestamp timestamp;
+
+  ActivityFeedItem({
+    this.username,
+    this.userId,
+    this.type,
+    this.mediaUrl,
+    this.postId,
+    this.userProfileImg,
+    this.commenData,
+    this.timestamp,
+  });
+
+  factory ActivityFeedItem.fromDocument(DocumentSnapshot doc) {
+    // print("inside factory");
+    // print(doc.data());
+    return ActivityFeedItem(
+      username: doc['username'],
+      userId: doc['userId'],
+      type: doc['type'],
+      mediaUrl: doc['mediaUrl'],
+      postId: doc['postId'],
+      userProfileImg: doc['userProfileImg'],
+      commenData: doc['comment'],
+      timestamp: doc['timestamp'],
+    );
+  }
+
+  configureMediaPreview() {
+    // print("configureMediaProfile");
+    if (type == "like" || type == "comment") {
+      mediaPreview = GestureDetector(
+        onTap: () => print("ShowPost"),
+        child: Container(
+          height: 50,
+          width: 50,
+          child: AspectRatio(
+            aspectRatio: 16 / 9,
+            child: Container(
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                fit: BoxFit.cover,
+                image: CachedNetworkImageProvider(mediaUrl),
+              )),
+            ),
+          ),
+        ),
+      );
+    } else {
+      mediaPreview = Text("");
+    }
+
+    if (type == 'like') {
+      activityItemText = "liked your post";
+    } else if (type == 'follow') {
+      activityItemText = "started following you";
+    } else if (type == 'comment') {
+      activityItemText = 'replied: $commenData';
+    } else {
+      activityItemText = "Something is fishy $type";
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text('Activity Feed Item');
+    configureMediaPreview();
+
+    return Padding(
+      padding: EdgeInsets.only(bottom: 2.0),
+      child: Container(
+        color: Colors.white54,
+        child: ListTile(
+          title: GestureDetector(
+            onTap: () => print("user Profile"),
+            child: RichText(
+              overflow: TextOverflow.ellipsis,
+              text: TextSpan(
+                  style: TextStyle(
+                    fontSize: 14.0,
+                    color: Colors.black,
+                  ),
+                  children: [
+                    TextSpan(
+                      text: username,
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    TextSpan(
+                      text: ' $activityItemText',
+                    ),
+                  ]),
+            ),
+          ),
+          leading: CircleAvatar(
+            backgroundColor: Colors.grey,
+            backgroundImage: CachedNetworkImageProvider(userProfileImg),
+          ),
+          subtitle: Text(
+            timeago.format(timestamp.toDate()),
+            overflow: TextOverflow.ellipsis,
+          ),
+          trailing: mediaPreview,
+        ),
+      ),
+    );
   }
 }
