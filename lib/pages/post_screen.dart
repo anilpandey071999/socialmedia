e1faeeac0b2b5d7f078ff5beb461920593d3edac
anilpandey071999 <31802461+anilpandey071999@users.noreply.github.com>
2021-01-17 01:26:13 +0530

    post screen

--- lib/pages/post_screen.dart
+++ lib/pages/post_screen.dart
@@ -1,8 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:socalnetwork/pages/home.dart';
+import 'package:socalnetwork/widgets/header.dart';
+import 'package:socalnetwork/widgets/post.dart';
+import 'package:socalnetwork/widgets/progress.dart';
 
 class PostScreen extends StatelessWidget {
+  final String userId;
+  final String postId;
+
+  PostScreen({
+    this.userId,
+    this.postId,
+  });
   @override
   Widget build(BuildContext context) {
-    return Text("Post Screen");
+    return FutureBuilder(
+        future: postsRef.doc(userId).collection(postId).get(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return linearProgress();
+          }
+          Post post = Post.fromDocument(snapshot.data);
+          return Center(
+            child: Scaffold(
+              appBar: header(
+                context,
+                titleText: post.description,
+              ),
+            ),
+          );
+        });
   }
 }
